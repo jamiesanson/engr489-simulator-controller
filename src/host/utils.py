d53030a0a94593aa8e52c727ad395e38e75e863b
Jamie Sanson <jamie.h.sanson@gmail.com>
2017-08-16 16:24:56 +1200

    Use shlex for arg flexibility

--- src/host/utils.py
+++ src/host/utils.py
@@ -1,4 +1,5 @@
 import curses
+import shlex
 
 class Command:
     def __init__(self, name, help, args = ["No arguments"]):
@@ -7,11 +8,10 @@ class Command:
         self.args = args
 
 def split_input(input_command):
-    cmd = input_command.split(' ')
-    decomposed = {'cmd': cmd[0]}
-    
+    args = shlex.split(input_command)
+    decomposed = {'cmd': args[0]}
     prev_arg = ''
-    for i, v in enumerate(cmd[1:]):
+    for i, v in enumerate(args[1:]):
         # If the index of the current thing being parsed is even it indicates it's the argument name, 
         # odd therefore indicates it's the argument value
         if i%2 == 0:
